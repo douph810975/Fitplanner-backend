6354631ce05724f0367386db82f384351f2b366b
douph810975 <pdou@connect.ust.hk>
2024-08-20 19:22:20 +0800

    bug

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.backend.repository")
+//@EnableJpaRepositories(basePackages = "com.example.backend.repository")
 public class BackendApplication {
 
     public static void main(String[] args) {
