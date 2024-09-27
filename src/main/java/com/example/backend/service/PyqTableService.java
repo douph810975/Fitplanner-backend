d9b2adf91de7a108e63a1670df02ace02d138b41
douph810975 <pdou@connect.ust.hk>
2024-09-27 01:25:49 -0700

    add PYQ

---
+++ src/main/java/com/example/backend/service/PyqTableService.java
@@ -0,0 +1,19 @@
+package com.example.backend.service;
+
+import com.example.backend.entity.Pyq;
+import com.example.backend.repository.PyqTableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PyqTableServcice {
+
+    @Autowired
+    private PyqTableRepository repository;
+
+    public List<Pyq> getAllPyqData() {
+        return repository.findAll();
+    }
+}
